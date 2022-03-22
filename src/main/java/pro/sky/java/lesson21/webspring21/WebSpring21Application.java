2d1f8c91a30402de2fa2558060637e4a22f13259
Ivan <uvanes21@yandex.ru>
2022-03-22 17:55:24 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/java/lesson21/webspring21/WebSpring21Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.lesson21.webspring21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpring21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSpring21Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/lesson21/webspring21/WebSpring21ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.lesson21.webspring21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSpring21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
